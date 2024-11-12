239d25fa96ccf678c46986c42ada37abdba466f3
SyedFazal110 <syedfazal808@gmail.com>
2024-11-12 20:22:14 +0500

    Updated

---
+++ InDrive-Clone/app/safety/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const safety = () => {
+  return (
+    <View>
+      <Text>safety</Text>
+    </View>
+  )
+}
+
+export default safety
